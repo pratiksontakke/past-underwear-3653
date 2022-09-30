7e4df77bd156425c504c42c18074b1204e98dd8f
Pratik Sontakke <pratiksontakke>
2022-09-30 14:51:43 +0530

    day-4

--- past-underwear-3653/src/com/masai/dao/CustomerDaoImpl.java
+++ past-underwear-3653/src/com/masai/dao/CustomerDaoImpl.java
@@ -2,9 +2,13 @@ package com.masai.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.masai.bean.Customer;
+import com.masai.exceptions.CustomerException;
 import com.masai.utility.DBUtil;
 
 public class CustomerDaoImpl implements CustomerDao {
@@ -52,6 +56,87 @@ public class CustomerDaoImpl implements CustomerDao {
 		
 		return message;
 
+	}
+
+	@Override
+	public Customer getCustomerByCid(int cid) throws CustomerException {
+		Customer customer = null;
+		
+		try(Connection conn = DBUtil.provideConnection()) {
+			
+			PreparedStatement ps = conn.prepareStatement("select * from customer where cid = ?");
+			
+			ps.setInt(1, cid);
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				rs.getInt("cid");
+				customer = new Customer(rs.getString("cname"), rs.getString("source"), rs.getString("destination"),rs.getString("busNo"));
+			} else {
+				throw new CustomerException("Customer does not exist with Id : " + cid);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace(); // this print at server side and client get null value
+			throw new CustomerException(e.getMessage()); // re-throw to client exception.
+		}
+		
+		return customer;
+	}
+
+	@Override
+	public Customer loginCustomer(int cid, String cname) throws CustomerException {
+		Customer customer = null;
+		
+		try(Connection conn = DBUtil.provideConnection()) {
+			
+			PreparedStatement ps = conn.prepareStatement("select * from customer where cid = ? AND cname = ?");
+			ps.setInt(1, cid);
+			ps.setString(2, cname);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				rs.getInt("cid");
+				customer = new Customer(rs.getString("cname"), rs.getString("source"), rs.getString("destination"),rs.getString("busNo"));
+			} else {
+				throw new CustomerException("Invalid username or password");
+			}
+			
+			
+		} catch (SQLException se) {
+			se.getMessage();
+			throw new CustomerException(se.getMessage());
+		}
+		
+		return customer;
+	}
+
+	@Override
+	public List<Customer> getAllCustomerDetails() throws CustomerException {
+		List<Customer> customers = new ArrayList<>();
+		
+		try(Connection conn = DBUtil.provideConnection()) {
+			PreparedStatement ps = conn.prepareStatement("select * from Customer");
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				rs.getInt("cid");
+				Customer customer = new Customer(rs.getString("cname"), rs.getString("source"), rs.getString("destination"),rs.getString("busNo"));
+				customers.add(customer);
+			}
+			
+			
+		} catch (Exception e) {
+			throw new CustomerException(e.getMessage());
+		}
+		
+		if(customers.size() == 0) {
+			throw new CustomerException("No Customers found...");
+		}
+		
+		return customers;
 	}
 
 	
